17da0fff661b2cfad3c9632801550a6d15b53be3
jake Fermin <jfermin345@gmail.com>
2019-10-18 22:10:31 +0200

    styling adjustments css october

---
+++ js/js-course1.js
@@ -0,0 +1,25 @@
+// console.log("new text");
+
+// var firstName = "jake";
+// var lastName = "Fermin";
+
+// console.log(firstName);
+
+// // console.log("yoo!! my name is" + firstName "and lastname is" + lastName);
+
+// console.log(firstName + ' yoo my name is ' + lastName);
+
+// alert("sup dude! welcome to the page pf the " + firstName);
+
+// prompt("what is your last name?");
+
+// var lastName = prompt("ur last name dude ");
+// alert("welcome" + lastName);
+// alert("sup dude! welcome to the page pf the " + firstName);
+
+
+// LECTURE 3
+var year, month, week;
+now = 2019;
+
+console.log(typeof now)
